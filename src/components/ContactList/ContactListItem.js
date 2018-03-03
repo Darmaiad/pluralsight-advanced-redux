d48560824e74cda448493cbbdf7a39677b167c44
George Filippakis <geo.filippakis@gmail.com>
2018-03-03 12:27:06 +0200

    Added event handler functions instead of inline functions

--- src/components/ContactList/ContactListItem.js
+++ src/components/ContactList/ContactListItem.js
@@ -4,23 +4,26 @@ import Chance from 'chance';
 
 import { OFFLINE } from './../../actions';
 
-const ContactListItem = ({ id, name, status, openChannel }) => (
-    <div>
-        <div className="media">
-            <div className="media-left">
-                <a href="#">
-                    <img className="media-object" style={{ width: 75, height: 75, backgroundColor: new Chance(id).color({ format: "rgb" }) }} />
-                </a>
-            </div>
-            <div className="media-body">
-                <h4 className="media-heading">{name}</h4>
-                {status !== OFFLINE ? <span className="btn btn-default" onClick={() => openChannel(id)} disabled={!status || status === OFFLINE}>
-                    Chat
+const ContactListItem = ({ id, name, status, openChannel }) => {
+    const handleChange = () => openChannel(id);
+
+    return (
+        <div>
+            <div className="media">
+                <div className="media-left">
+                    <a href="#">
+                        <img className="media-object" style={{ width: 75, height: 75, backgroundColor: new Chance(id).color({ format: "rgb" }) }} />
+                    </a>
+                </div>
+                <div className="media-body">
+                    <h4 className="media-heading">{name}</h4>
+                    {status !== OFFLINE ? <span className="btn btn-default" onClick={handleChange} disabled={!status || status === OFFLINE}>
+                        Chat
                 </span> : <span>(Offline)</span>}
+                </div>
             </div>
-        </div>
-    </div>
-);
+        </div>);
+};
 
 ContactListItem.propTypes = {
     id: PropTypes.string.isRequired,
